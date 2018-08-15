6c72adf48615d964541bae45db4e1c89a790f6d9
Douglas Ausech <dausech@gmail.com>
2018-08-15 08:36:54 -0300

    Tratar consulta cadastro SP

--- pynfe/processamento/comunicacao.py
+++ pynfe/processamento/comunicacao.py
@@ -199,7 +199,7 @@ class ComunicacaoSefaz(Comunicacao):
         # Monta XML para envio da requisição
         
         metodo = 'CadConsultaCadastro2'   
-        if self.uf.upper() == 'SP'
+        if self.uf.upper() == 'SP':
            metodo = 'CadConsultaCadastro4'        
            
         xml = self._construir_xml_status_pr(cabecalho=self._cabecalho_soap(metodo=metodo), metodo=metodo, dados=raiz)
